336ec00729997c1019c23a295db21e9e444bec61
evo-cyril <cyril.k@evoqins.com>
2025-10-29 11:16:06 +0530

    Alchemy queries

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 print("SQLAlchemy Tutorial")
+
+from models import *
+
+user1 = User(name="Alice", email="alice@example.com", age=25, country="India", salary=50000, is_active=True),
+user2 = User(name="Bob", email="bob@example.com", age=30, country="USA", salary=70000, is_active=True),
+user3 = User(name="Charlie", email="charlie@example.com", age=28, country="India", salary=60000, is_active=False),
+user4 = User(name="Diana", email="diana@example.com", age=35, country="UK", salary=90000, is_active=True),
+user5 = User(name="Ethan", email="ethan@example.com", age=22, country="India", salary=40000, is_active=True),
+
+session.add(user1)
+session.commit()
+
+db_records = session.query(User).all()
+
+for record in db_records:
+    print(record)
---
+++ models.py
@@ -0,0 +1,36 @@
+# Setup SQL connection here
+
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, Date, Float
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = "sqlite:///./database.db"
+
+
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+session = SessionLocal()
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False)
+    email = Column(String(100), unique=True, index=True)
+    age = Column(Integer)
+    country = Column(String(50))
+    salary = Column(Float)
+    is_active = Column(Boolean, default=True)
+    join_date = Column(Date, default=date.today)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, name='{self.name}', age='{self.age}', email='{self.email}')>"
+
+Base.metadata.create_all(bind=engine)
+
+
