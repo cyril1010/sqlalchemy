20bbd54113a92f9595e57be967faefa14f92a67e
evo-cyril <cyril.k@evoqins.com>
2025-10-30 11:30:57 +0530

    modifications

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ print("SQLAlchemy Tutorial")
 
 from models import session,User
 
-# user1 = User(name="Alice", email="alice@example.com", age=25, country="India", salary=50000, is_active=True)
-# user2 = User(name="Bob", email="bob@example.com", age=30, country="USA", salary=70000, is_active=True)
-# user3 = User(name="Charlie", email="charlie@example.com", age=28, country="India", salary=60000, is_active=False)
-# user4 = User(name="Diana", email="diana@example.com", age=35, country="UK", salary=90000, is_active=True)
-# user5 = User(name="Ethan", email="ethan@example.com", age=22, country="India", salary=40000, is_active=True)
+user1 = User(name="Alice", email="alice@example.com", age=25, country="India", salary=50000, is_active=True)
+user2 = User(name="Bob", email="bob@example.com", age=30, country="USA", salary=70000, is_active=True)
+user3 = User(name="Charlie", email="charlie@example.com", age=28, country="India", salary=60000, is_active=False)
+user4 = User(name="Diana", email="diana@example.com", age=35, country="UK", salary=90000, is_active=True)
+user5 = User(name="Ethan", email="ethan@example.com", age=22, country="India", salary=40000, is_active=True)
 
-# session.add(user1)
-# session.add(user2)
-# session.add(user3)
+session.add(user1)
+session.add(user2)
+session.add(user3)
+session.add(user4)
+session.add(user5)
 
-# session.commit()
+
+session.commit()
 
 db_records = session.query(User).all()
 
--- models.py
+++ models.py
@@ -4,8 +4,8 @@ from sqlalchemy import create_engine, Column, Integer, String, Boolean, Date, Fl
 from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import date
 
-# DATABASE_URL = "sqlite:///./database.db"
-DATABASE_URL = 'postgresql://postgres:evoqins2025@localhost:5432/cyb'
+DATABASE_URL = "sqlite:///./database.db"
+# DATABASE_URL = 'postgresql://postgres:evoqins2025@localhost:5432/cyb'
 
 
 
